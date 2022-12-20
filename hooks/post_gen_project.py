42df30344f4bc1c9d87b22339885bc2d70d65afb
nj.ara1995 <nj.ara1995@gmail.com>
2022-12-20 17:32:26 -0500

    refactor: change directory structure of gradle build

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,28 +6,26 @@ if '{{ cookiecutter.license }}' != 'Unlicensed':
     open('LICENSE.md', 'w').write(open('licenses/{{ cookiecutter.license }}','r').read())
 shutil.rmtree('licenses')
 
+ver_syntax = '\n[[tool.proman.versioning.files]]\ncompat = "semver"'
+
 if '{{ cookiecutter.build_system }}' == 'Maven':
-    source = 'app/'
-    allfiles = os.listdir(source)
-    for f in allfiles:
-        src_path = os.path.join(source, f)
-        dst_path = os.path.join('.', f)
-        shutil.move(src_path, dst_path)
-    shutil.rmtree(source)
     all_delete_files = glob.glob(os.path.join('.', '*gradle*'), recursive=True)
     for f in all_delete_files:
         shutil.rmtree(f) if os.path.isdir(f) else os.remove(f)
     open('.versioning', 'a+').write(
-        '\n[[tool.proman.versioning.files]]\
-        \ncompat = "semver"\
+        '{}\
         \nfilepath = "pom.xml"\
-        \npattern = "<version>${version}</version>"'
+        \npattern = "<version>{}</version>"\
+        '.format(ver_syntax, '${version}')
     )
 else:
     os.remove('pom.xml')
     open('.versioning', 'a+').write(
-        '\n[[tool.proman.versioning.files]]\
-        \ncompat = "semver"\
+        '{}\
         \nfilepath = "gradle.properties"\
-        \npattern = "version=${version}"'
+        \npattern = "version={}"\
+        \n{}\
+        \nfilepath = "build.gradle"\
+        \npattern = "version = {}"\
+        '.format(ver_syntax, '${version}', ver_syntax, '${version}')
     )
